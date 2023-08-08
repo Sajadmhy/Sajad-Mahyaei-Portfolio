923c252df894c09b1da05e52f1d186d996536d07
sajadmhy <sajad.mahyaei@gmail.com>
2023-08-08 19:27:11 +0330

    update email to .tech

--- pages/index.jsx
+++ pages/index.jsx
@@ -70,7 +70,7 @@ export default function Home() {
             />
         </a>
 
-        <a href="mailto:hi@sajadm.me">
+        <a href="mailto:hi@sajadm.tech">
           <Image 
             src="/email.webp"
             width={40}
